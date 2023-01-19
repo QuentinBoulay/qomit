3fa8b2e248d3f8432601c87ac0f3cf6c4e360853
Nobh <nadir607@hotmail.fr>
2023-01-19 11:24:08 +0100

    Volumes + Mysql

--- backend/server.js
+++ backend/server.js
@@ -9,3 +9,23 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
+
+
+const mysql = require('mysql2');
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'yourdatabasename'
+});
+
+// simple query
+connection.query(
+  'SELECT * FROM `table_name`',
+  (err, results, fields) => {
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  }
+);
