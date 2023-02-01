548f03f1f409f980ad8a0839aea4c740d24bca85
QuentinBoulay <63411603+QuentinBoulay@users.noreply.github.com>
2023-02-01 12:37:29 +0100

    Correction menu responsive + debut création bloc "services"

--- frontend/src/components/client/Home/Home.js
+++ frontend/src/components/client/Home/Home.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import Nav from '../Templates/Nav.js';
+import Hero from './Hero.js';
 
 function Home ()  {
 
   return (
     <>
     <Nav/>
-
-    <div className='hero text-center'>
-      <h1>Suis ton projet web<br></br> de <span>A à Z.</span></h1>
-      <button className="mx-auto mt-5 purple-button d-flex align-items-center">
-      Demande de devis
-      <box-icon name='right-arrow-alt' color='#ffffff' ></box-icon>
-      </button>
-      <div className="d-flex mt-5 justify-content-center align-items-center">
-        <a href="/"><box-icon name='linkedin' type='logo' color='#5959eb' style={{ marginRight: "20px", width: "50px", height: "50px" }} ></box-icon></a>
-        <a href="/"><box-icon name='instagram' type='logo' color='#5959eb' style={{ width: "50px", height: "50px" }}  ></box-icon></a>
-      </div>
-      <div className='video-container mx-auto d-flex justify-content-center align-items-center'>
-        <box-icon name='play' color='#5959eb' style={{ width: "100px", height: "100px" }} ></box-icon>
-      </div>
-        
+    <Hero/>
+    <div className='services'>
+      <h2 className='font-size-82 text-center'>Nos services</h2>
     </div>
     </>
   );
--- frontend/src/components/client/Templates/Nav.js
+++ frontend/src/components/client/Templates/Nav.js
@@ -1,74 +1,79 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import logo from "../../../img/logo.png";
 
 function Nav ()  {
   const [isActive, setIsActive] = useState(false);
 
+  useEffect(() => {
+    document.body.style.overflow = isActive ? 'hidden' : 'visible';
+  }, [isActive]);
+
   const handleClick = () => {
     setIsActive(prevState => !prevState);
   };
 
   return (
+    
     <div className='container-fluid'>
         <nav>
           <div className='row py-4 px-5 align-items-center'>
-            <div className='logo-container p-0 d-flex justify-content-start col-12 col-lg-3 col-md-1 col-sm-1'>
+            <div className='logo-container p-0 d-flex justify-content-start col-12 col-lg-3 col-md-6 col-sm-6'>
               <Link to="/">
                 <img className='w-50' src={logo} alt='logo QOMIT'></img>
               </Link>
             </div>
-            <div className='menu-container p-0 col-12 col-lg-6 col-md-8 d-none d-md-block'>
+            <div className='menu-container p-0 col-12 col-lg-6 col-md-6 d-none d-lg-block'>
                 <ul className='list-unstyled d-flex justify-content-center'>
                   <div className='d-flex'>
-                    <li className='mr-1 font-size-18 active'>
+                    <li className='mr-1 font-size-14 active'>
                       <Link className='active' to="/admin/posts">Accueil</Link>
                     </li>
-                    <li className='mr-1 font-size-18'>
+                    <li className='mr-1 font-size-14'>
                       <Link to="/admin/posts">Services</Link>
                     </li>
-                    <li className='mr-1 font-size-18'>
+                    <li className='mr-1 font-size-14'>
                       <Link to="/admin/posts">L'agence</Link>
                     </li>
-                    <li className='font-size-18'>
+                    <li className='font-size-14'>
                       <Link to="/admin/posts">Devis</Link>
                     </li>
                   </div>
                 </ul>
               </div>
-              <div className="mobile-nav-button d-block d-sm-none" onClick={handleClick}>
+              <div className="mobile-nav-button d-md-block d-sm-block d-xs-block d-lg-none" onClick={handleClick}>
                 <div className={`mobile-nav-button__line ${isActive ? 'mobile-nav-button__line--1 line_white' : ''}`}></div>
                 <div className={`mobile-nav-button__line ${isActive ? 'mobile-nav-button__line--2 line_white' : ''}`}></div>
                 <div className={`mobile-nav-button__line ${isActive ? 'mobile-nav-button__line--3 line_white' : ''}`}></div>
               </div>
 
-              <nav className={`mobile-menu ${isActive ? 'mobile-menu--open d-block d-sm-none' : ''}`}>
+              <nav className={`mobile-menu ${isActive ? 'mobile-menu--open d-md-block d-sm-block d-xs-block' : ''}`}>
                 <ul className='list-unstyled'>
                   <li className='mr-1 font-size-48 active'>
-                    <Link className='active' to="/admin/posts">Accueil</Link>
+                    <Link className='d-inline-block active' to="/admin/posts">Accueil</Link>
                   </li>
                   <li className='mr-1 font-size-48'>
-                    <Link to="/admin/posts">Services</Link>
+                    <Link className="d-inline-block" to="/admin/posts">Services</Link>
                   </li>
                   <li className='mr-1 font-size-48'>
-                    <Link to="/admin/posts">L'agence</Link>
+                    <Link className="d-inline-block" to="/admin/posts">L'agence</Link>
                   </li>
                   <li className='font-size-48'>
-                    <Link to="/admin/posts">Devis</Link>
+                    <Link className="d-inline-block" to="/admin/posts">Devis</Link>
                   </li>
 
                   <li className='mt-5 font-size-48'>
-                  <Link to="/admin/posts">Contact</Link>
+                  <Link className="d-inline-block" to="/admin/posts">Contact</Link>
                   </li>
                   <li className='mt-5 text-center font-size-28'>
                     <div className='client-place-burger d-inline-block'><Link to="/admin/posts">Espace client</Link></div>
                   </li>
                 </ul>
               </nav>
-              <div className='contact-container p-0 d-flex justify-content-end align-items-center col-12 col-lg-3 col-md-3'>
-                <Link className='d-none d-sm-block font-size-18' to="/admin/posts">Contact</Link>
-                <div className='d-none d-sm-block ml-4 client-place button'>
-                  <Link className='font-size-18' to="/admin/posts">Espace client</Link>
+              <div className='contact-container p-0 d-flex justify-content-end align-items-center col-12 col-lg-3'>
+                <Link className='d-none d-lg-block font-size-14' to="/admin/posts">Contact</Link>
+                <div className='d-none d-lg-block ml-4 client-place button'>
+                  <Link className='font-size-14' to="/admin/posts">Espace client</Link>
                 </div>
               </div>
             </div>
