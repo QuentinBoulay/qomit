c2d5ba9fc2d3a6bb37db6127c457b99e826ef2c9
QuentinBoulay <quentinboulay2000@gmail.com>
2023-02-28 12:31:41 +0100

    Modif page d'accueil

--- frontend/src/components/client/Home/Home.js
+++ frontend/src/components/client/Home/Home.js
@@ -1,34 +1,41 @@
-import React from 'react';
-import Nav from '../Templates/Nav.js';
-import Hero from './Hero.js';
+import Nav from "../Templates/Nav.js";
+import Hero from "./Hero.js";
+import Services from "./Services.js";
+import Ticker from "./Ticker.js";
 
-function Home ()  {
-
+function Home() {
   return (
     <>
-    <Nav/>
-    <Hero/>
-    <div className='services'>
-      <h2 className='font-size-82 text-center'>Nos <div className='underline-container d-inline-block px-3'>services</div></h2>
-      <div className='row justify-content-between'>
-        <div className='description-service col-12 col-lg-6'>
-          <p>01.</p>
-          <lottie-player src="https://lottie.host/c0890e44-a909-4372-b769-660d161f67a9/hLjYM7mcxj.json" background="transparent" speed="1" style={{width: '500px', height: '500px', margin: '0 auto'}} loop autoplay></lottie-player>
-          <h3 className='mt-5'>Développement web</h3>
-          <p>Lorem ipsum dolor sit amet. Praesent eu est a velit viverra aliquam.</p>
-        </div>
-        <div className='group-buttons-services col-12 col-lg-4'>
-          <div className='input-container d-flex justify-content-end align-items-center'>
-            <p>01.</p>
-            <h3>Développement web</h3>
-            <box-icon name='plus' color='#ffffff' ></box-icon>
-          </div>
-        </div>
+      <Nav />
+      <Hero />
+      <Services />
+      <Ticker />
+      <div className="devis-container position-relative">
+        <h2 className="w-50 mb-5">
+          Lorem ipsum dolor sit amet. Etiam efficitur ipsum, a vehicula porta.
+          Ut vitae egestas ante.
+        </h2>
+        <button className="purple-button d-flex align-items-center">
+          Demande de devis
+          <box-icon name="right-arrow-alt" color="#ffffff"></box-icon>
+        </button>
+        <lottie-player
+          src="https://lottie.host/2701362e-b03e-41fd-961c-b5277dbc789b/aCvD8DkmxL.json"
+          background="transparent"
+          speed="1"
+          style={{
+            width: "400px",
+            height: "400px",
+            position: "absolute",
+            bottom: "-42px",
+            right: "100px",
+          }}
+          loop
+          autoplay
+        ></lottie-player>
       </div>
-    </div>
     </>
   );
-
-};
+}
 
 export default Home;
