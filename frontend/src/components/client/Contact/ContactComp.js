fd2b0fcb82ade592f44cb42cbb929b3aaa017790
Nobh <nadir607@hotmail.fr>
2023-04-06 19:18:10 +0200

    UPDATE: Contact page

--- frontend/src/components/client/Contact/ContactComp.js
+++ frontend/src/components/client/Contact/ContactComp.js
@@ -1,7 +1,9 @@
+import NavDark from "../Templates/NavDark"
 import Contact from "../Templates/Contact.js";
 import Footer from "../Templates/Footer.js";
 import emailjs from "emailjs-com";
 import { useState } from "react";
+import HeroContact from "./HeroContact";
 
 function ContactComp() {
   const [showNotification, setShowNotification] = useState(false);
@@ -55,6 +57,8 @@ function ContactComp() {
   return (
     <>
       <div className="body-contact">
+        <NavDark />
+        <HeroContact />
         {showNotification && (
           <div className={`notification ${notificationType}`}>
             <p>{notificationMessage}</p>
---
+++ frontend/src/components/client/Contact/HeroContact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function HeroContact() {
+  return (
+    <div className="hero-dark d-flex flex-column">
+        <h1>Nous {" "}
+          <div className="underline-container d-inline-block px-3">
+            contacter
+          </div>
+          !
+        </h1>
+        <p className="hero-dark-subtitle">Expliquez nous votre projet.</p>
+    </div>
+  );
+}
+
+export default HeroContact;
